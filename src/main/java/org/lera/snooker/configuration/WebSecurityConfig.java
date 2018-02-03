365b4a09507e12d2de35e9bb05055a813f2bbee5
Leandro Conte <leandro.dev77@gmail.com>
2018-02-02 23:04:45 -0200

    CSRF disabled

--- src/main/java/org/lera/snooker/configuration/WebSecurityConfig.java
+++ src/main/java/org/lera/snooker/configuration/WebSecurityConfig.java
@@ -13,6 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
                     .anyRequest().authenticated()
